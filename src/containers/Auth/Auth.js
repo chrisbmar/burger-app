04efea9a506a794718af562de5f54027cb973960
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-01-13 11:22:38 +0100

    setup actions for auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
+import { connect } from 'react-redux';
 
 class Auth extends Component {
     state = {
@@ -71,6 +73,11 @@ class Auth extends Component {
        };
        this.setState({controls: updatedControls});
     }
+
+    submitHanlder = (event) => {
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    }
     
     render () {
         const formElementsArray = [];
@@ -95,14 +102,19 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-               <form>
+               <form onSubmit={this.submitHanlder}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                </form> 
             </div>
-
         );
     }
 }
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+};
+
+export default connect(null, mapDispatchToProps)(Auth);
