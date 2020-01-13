468284de0b899b6cb757453d3b956574f20b6708
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-01-13 10:59:48 +0100

    authorization form

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ const button = (props) => (
     <button
         disabled={props.disabled}
         className={[classes.Button, classes[props.btnType]].join(' ')}
-        onClick={props.clicked}>{props.btnType}</button>
+        onClick={props.clicked}>{props.children}</button>
 );
 
 export default button;
