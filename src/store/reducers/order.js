098490a08cb9e9ce51626d3b4a08e34951ceeb7e
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-02-15 10:33:01 +0100

    use update object helper function in the whole app

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -1,6 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
-import { updateObject } from '../utility';
-import { purchaseBurger } from '../actions';
+import { updateObject } from '../../shared/utility';
 
 const initialState = {
     orders: [],
