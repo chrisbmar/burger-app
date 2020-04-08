83e676f3f10f36ca81e1c2ed2f975f0946f77e56
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-04-08 17:23:22 +0200

    moving logic from an action creator to a saga

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 import thunk from "redux-thunk";
 import createSagaMiddleware from "redux-saga";
-import { logoutSaga } from "./store/sagas/auth";
+import { watchAuth } from "./store/sagas/index";
 
 const composeEnhancers =
   process.env.NODE_ENV === "development"
@@ -31,7 +31,7 @@ const store = createStore(
   composeEnhancers(applyMiddleware(thunk, sagaMiddleware))
 );
 
-sagaMiddleware.run(logoutSaga);
+sagaMiddleware.run(watchAuth);
 
 const app = (
   <Provider store={store}>
